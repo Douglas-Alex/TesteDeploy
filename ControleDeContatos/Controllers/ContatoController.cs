e49d7f9ca8c19ed33424e4205c7d6c468396fdc8
Douglas Alex <douglasalex.dab@gmail.com>
2022-04-17 10:37:44 -0300

    Cadastro de contatos

--- ControleDeContatos/Controllers/ContatoController.cs
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -20,9 +20,10 @@ namespace ControleDeContatos.Controllers
         {
             return View();
         }
-        public IActionResult Editar()
+        public IActionResult Editar(int id)
         {
-            return View();
+            ContatoModel contato = _contatoRepositorio.ListarPorId(id);
+            return View(contato);
         }
         public IActionResult ApagarConfirmacao()
         {
@@ -34,6 +35,14 @@ namespace ControleDeContatos.Controllers
         {
             _contatoRepositorio.Adicionar(contato);
             return RedirectToAction("Index");   
+
+        }
+
+        [HttpPost]
+        public IActionResult Alterar(ContatoModel contato)
+        {
+            _contatoRepositorio.Atualizar(contato);
+            return RedirectToAction("Index");
 
         }
     }
--- ControleDeContatos/Repositorio/ContatoRepositorio.cs
+++ ControleDeContatos/Repositorio/ContatoRepositorio.cs
@@ -14,11 +14,16 @@ namespace ControleDeContatos.Repositorio
             _bancoContext = bancoContext;
         }
 
+        public ContatoModel ListarPorId(int id)
+        {
+           return _bancoContext.Contatos.FirstOrDefault(x => x.Id == id);
+        }
 
         public List<ContatoModel> BuscarTodos()
         {
             return _bancoContext.Contatos.ToList();
         }
+
         public ContatoModel Adicionar(ContatoModel contato)
         {
             _bancoContext.Contatos.Add(contato);
@@ -26,5 +31,20 @@ namespace ControleDeContatos.Repositorio
             return contato;
         }
 
+        public ContatoModel Atualizar(ContatoModel contato)
+        {
+            ContatoModel contatoDB = ListarPorId(contato.Id);
+
+            if (contatoDB == null) throw new System.Exception("Houve um erro na atualização do contato!");
+
+            contatoDB.Nome = contato.Nome;
+            contatoDB.Email = contato.Email;
+            contatoDB.Telefone = contato.Telefone;
+
+            _bancoContext.Contatos.Update(contatoDB);
+            _bancoContext.SaveChanges();
+
+            return contatoDB;
+        }
     }
 }
